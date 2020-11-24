7cf6250267c9e82424176f9eee7ec1bd32d31b1c
David Mohl <git@davemail.io>
2020-11-24 18:36:24 +0900

    Rename lg-netcast -> netcast-tv

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'LgNetcast';
+export const PLATFORM_NAME = 'netcasttv';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-lg-netcast';
+export const PLUGIN_NAME = 'homebridge-netcast-tv';
